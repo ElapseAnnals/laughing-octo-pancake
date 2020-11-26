dd7e884a93a2580dcf5846e5d52487bf291c3aa1
Ben.Huang <benhuang1024@gmail.com>
2020-11-26 12:57:21 +0800

    'init'

---
+++ src/main/java/com/example/laughingoctopancake/LaughingOctoPancakeApplication.java
@@ -0,0 +1,13 @@
+package com.example.laughingoctopancake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaughingOctoPancakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaughingOctoPancakeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/laughingoctopancake/LaughingOctoPancakeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.laughingoctopancake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaughingOctoPancakeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
